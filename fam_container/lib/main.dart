4de53f9a5e6caf03517b132cb0662481bcc990fd
Rishabh <rishabh@Apples-MacBook-Pro.local>
2025-04-29 09:36:24 +0530

    Presentation layer UI

--- fam_container/lib/main.dart
+++ fam_container/lib/main.dart
@@ -1,68 +1,47 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'core/injection/injection.dart';
+import 'features/fam_container/presentation/home_page.dart';
+
+void main() => mainCommon();
+
+Future<void> mainCommon() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const FamPay());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class FamPay extends StatefulWidget {
+  const FamPay({super.key});
+
+  @override
+  State<FamPay> createState() => _FamPayState();
+}
+
+class _FamPayState extends State<FamPay> {
+  @override
+  void initState() {
+    registerOnAppStartUpServices();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    unRegisterOnAppClosedServices();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'FamPay',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: const MyHomePage(title: 'FamPay 🚀'));
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+      title: 'FamPay',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'Satoshi',
+        useMaterial3: true,
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      home: HomePage(),
     );
   }
 }
